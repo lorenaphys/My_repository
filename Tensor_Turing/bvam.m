21777f3dffd9234e765b2c1244c8ba7ca9fad0f4
lorenaphys <lorena.030789@gmail.com>
2016-02-26 20:17:02 -0600

    aun falta darle un valor a C

--- Tensor_Turing/bvam.m
+++ Tensor_Turing/bvam.m
@@ -18,6 +18,9 @@ u2 = 1;
 u3 = 0;
 beta = 0.1;
 L = -10;
+alpha = 1e-2;
+dt = 1e-5;
+dt1 = 0.02;
 
 %Parametros del modelo BVAM
 
@@ -54,14 +57,77 @@ for i=1:Nx
    end
 end
 
-%Condicion inicila para el morfogeno
+%Condicion inicial para el morfogeno
 
-N = 3;
+%N = 3;
 [X,Y,Z]=meshgrid(1:Nx,1:Ny,1:Nz);
-teta=atan2((Y-Ny/2),(X-Nx/2));
-rad=sqrt((X-Nx/2+.5).^2+(Y-Ny/2+.5).^2);
-%u=1.5*exp(-((X-Nx/2-.5).^2+(Y-Ny/2-.5).^2+(Z-R+2).^2)/100);
+%teta=atan2((Y-Ny/2),(X-Nx/2));
+%rad=sqrt((X-Nx/2+.5).^2+(Y-Ny/2+.5).^2);
+u=1.5*exp(-((X-Nx/2-.5).^2+(Y-Ny/2-.5).^2+(Z-7).^2)/50);
 %u=2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((-X+Nx/2-.5).^2+(-Y+Ny/2-.5).^2+(-Z+R+14).^2)/80));
-u=-2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((X-Nx/2).^2+(Y-Ny/2).^2+(Z-1).^2)/50));
+%u=-2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((X-Nx/2).^2+(Y-Ny/2).^2+(Z-1).^2)/50));
 %u=2.5*rand(Nx,Ny,Nz);
+
+%iteraciones del modelo
+    %Variables que guardan todas la iteraciones
+Fm = zeros(Nx,Ny,Nz,NF+1);
+Um = zeros(Nx,Ny,Nz,NF+1);
+Fm(:,:,:,1) = fi;
+Um(:,:,:,1) = u;
+
+%funcion que contabiliza el timepo de proceso
+t = tic();
+
+for i = 1:NF
+   for j = 1:step
+       
+      lapfi = lapf3D(fi); 
+      %potencial quimico
+      mu = (fi.^2-1).*(fi-ep*beta*u)-ep^2*lapfi;
+      
+      %variacion de la energia libre con respecto a fi
+      lapmu = lapf3D(mu);
+      varFfi = 2*Afi*mu.*(3*fi.^2-1-2*ep*beta*fi.*u) + 4*As*fi.*(fi.^2-1).*(u-u1).^2.*(u-u2).^2 + 2*Af*fi.*(u-u3).^2 - ...
+               2*sigma*lapfi - 2*Afi*ep^2*lapmu;
+      
+      %implementacion del modelo bvam
+      u=.1*u+.2*(rand(Nx,Ny,Nz)-.5);
+      v=.1*u+.2*(rand(Nx,Ny,Nz)-.5);
+      
+      lapu = lapf3D(u);
+      lapv = lapf3D(v);
+      u = u + dt1*(D*lapu + eta1*(u+a*v-C*u.*v-u.*v.^2));
+      v = v + dt1*(lapv + eta1*(b*v+h*u+C*u.*v+u.v.^2));
+           
+      %variacion de la energia libre con respecto a u
+      lapu = lapf3D(u);
+      varFu = -2*Afi*ep*beta*mu.*(fi.^2-1) + 2*As*(fi.^2-1).^2.*(2*u-u1-u2) + 2*Af*fi.^2.*(u-u3) - 2*Af*L*lapu;
+      
+      %crecimiento de fi debido a la sustancia
+      lapFu = lapf3D(varFu);
+      I = lapFu*sum(sum(sum(fi >= -0.99)));
+      
+      %dinamica de los campos fase
+      lapFfi = lapf3D(varFfi);
+      fi = fi + Dfi*dt*(lapFfi + alpha*I);
+      u = u + Du*dt*lapFu;
+      
+      %condiciones de frontera
+      fi(:,:,1) = fi(:,:,2);
+      u(:,:,1) = u(:,:,2);
+      
+      %condicion para parar el proceso en caso de que fi tenga entradas
+      %tipo NaN
+      k=isnan(fi(Nx/2,Ny/2,Nz/2));
+      if k == 1;
+          break
+      end
+   end
+   Fm(:,:,:,i+1) = fi;
+   Um(:,:,:,i+1) = u;
+end
+
+time = toc(t);
+
+save('feb26a');
                                                                                                                                

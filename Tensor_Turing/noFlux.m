c027cd54cd9ae2fba3282d84808fbae4b4dc7f9b
lorenaphys <lorena.030789@gmail.com>
2016-04-14 18:05:52 -0500

    definiendo de nuevo las condiciones de frontera

--- Tensor_Turing/noFlux.m
+++ Tensor_Turing/noFlux.m
@@ -5,12 +5,20 @@ Nx = h(1);
 Ny = h(2);
 Nz = h(3);
 
-for i = 2:Nx
-   for j = 2:Ny
-      for k = 2:Nz
-         if abs(f(i,j,k)) <= 0.01
-             f(i,j,k) = f(i-1,j-1,k-1); 
-         end
+for i = 1:Nx
+   for j = 1:Ny
+      for k = 1:Nz
+        if f == 0
+           if (i < Nx) || (j < Ny) || (k < Nz) 
+               f(i,j,k) = f(i+1,j+1,k+1);
+           elseif i == Nx
+               f(i,j,k) = f(i-1,j,k);
+           elseif j == Ny
+               f(i,j,k) = f(i,j-1,k);
+           else
+               f(i,j,k) = f(i,j,k-1);
+           end
+        end
       end
    end
 end

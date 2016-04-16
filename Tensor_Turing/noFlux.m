6037ad1d78d4ab138b48160052f8bb1e2cf7fef7
lorenaphys <lorena.030789@gmail.com>
2016-04-15 22:09:42 -0500

    tercera correccion

--- Tensor_Turing/noFlux.m
+++ Tensor_Turing/noFlux.m
@@ -8,7 +8,7 @@ Nz = h(3);
 for i = 1:Nx
    for j = 1:Ny
       for k = 1:Nz
-         if abs(fi(i,j,k)) <= 0.1
+         if abs(f(i,j,k)) <= 0.1
              if i == 1
                  f(i+1,j,k) = 0;
              else

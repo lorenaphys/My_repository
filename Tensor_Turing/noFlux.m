199774b58f1e9366bb7e70525c222253a0b55b8d
lorenaphys <lorena.030789@gmail.com>
2016-04-15 21:57:57 -0500

    nuevos cambios a las condiciones de frontera

--- Tensor_Turing/noFlux.m
+++ Tensor_Turing/noFlux.m
@@ -8,17 +8,11 @@ Nz = h(3);
 for i = 1:Nx
    for j = 1:Ny
       for k = 1:Nz
-        if f == 0
-           if (i < Nx) || (j < Ny) || (k < Nz) 
-               f(i,j,k) = f(i+1,j+1,k+1);
-           elseif i == Nx
-               f(i,j,k) = f(i-1,j,k);
-           elseif j == Ny
-               f(i,j,k) = f(i,j-1,k);
-           else
-               f(i,j,k) = f(i,j,k-1);
-           end
-        end
+         if f(i,j,k) == 0
+             f(i-1,j,k) = 0;
+             f(i,j-1,k) = 0;
+             f(i,j,k-1) = 0;
+         end
       end
    end
 end

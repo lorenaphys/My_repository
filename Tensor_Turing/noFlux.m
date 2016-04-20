de4efc05748a45b730e1a5166c5e16bd93ee6ebe
lorenaphys <lorena.030789@gmail.com>
2016-04-20 18:26:33 -0500

    espero esto de resultados

--- Tensor_Turing/noFlux.m
+++ Tensor_Turing/noFlux.m
@@ -10,21 +10,21 @@ for i = 1:Nx
       for k = 1:Nz
          if abs(f(i,j,k)) <= 0.1
              if i == 1
-                 f(i+1,j,k) = 0;
+                 f(i+1,j,k) = f(i,j,k);
              else
-                 f(i-1,j,k) = 0;
+                 f(i-1,j,k) = f(i,j,k);
              end
              
              if j == 1
-                 f(i,j+1,k) = 0;
+                 f(i,j+1,k) = f(i,j,k);
              else
-                 f(i,j-1,k) = 0;
+                 f(i,j-1,k) = f(i,j,k);
              end
              
              if k == 1
-                 f(i,j,k+1) = 0;
+                 f(i,j,k+1) = f(i,j,k);
              else
-                 f(i,j,k-1) = 0;
+                 f(i,j,k-1) = f(i,j,k);
              end
          end
       end

2b5df82365d90608eab5057a2675b31f8940bbaa
lorenaphys <lorena.030789@gmail.com>
2016-06-30 19:13:44 -0500

    NF=300

--- Tensor_Turing/graphics.m
+++ Tensor_Turing/graphics.m
@@ -1,12 +1,12 @@
 
-load('prueba.mat')
+load('junio30f.mat')
 
 
 fl=size(Fm);
 Nx = fl(1);
 Ny = fl(2);
 Nz = fl(3);
-f = 2;
+f = 201;
 M = struct('cdata',[],'colormap',[]);
 fi0(:,:,:) = Fm(:,:,:,1);
 fi(:,:,:)=Fm(:,:,:,f);

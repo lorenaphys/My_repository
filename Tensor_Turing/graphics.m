a79670849d2650eb4cedad0eb3c8a46db49dac3a
lorenaphys <lorena.030789@gmail.com>
2016-07-02 16:39:44 -0500

    fi0:esfera uo:gaussiana con h=1.5 sigma^2=40 y x=(20,20,35)

--- Tensor_Turing/graphics.m
+++ Tensor_Turing/graphics.m
@@ -1,12 +1,12 @@
 
-load('julio2b.mat')
+load('prueba.mat')
 
 
 fl=size(Fm);
 Nx = fl(1);
 Ny = fl(2);
 Nz = fl(3);
-f = 151;
+f = 2;
 M = struct('cdata',[],'colormap',[]);
 fi0(:,:,:) = Fm(:,:,:,1);
 fi(:,:,:)=Fm(:,:,:,f);

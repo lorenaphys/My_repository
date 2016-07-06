635c2b48cb8120b97ccaab8f8c5fc359e2008de3
lorenaphys <lorena.030789@gmail.com>
2016-07-06 15:26:19 -0500

    fi0:domo u0:rompimiento de simetria con N=3

--- 3D/figure3D.m
+++ 3D/figure3D.m
@@ -1,11 +1,11 @@
 
-load('julio6g.mat')
+load('julio6h.mat')
 
 fl=size(Fm);
 Nx = fl(1);
 Ny = fl(2);
 Nz = fl(3);
-f = 201;
+f = 156;
 M = struct('cdata',[],'colormap',[]);
 fi(:,:,:)=Fm(:,:,:,f);
 u(:,:,:)=Um(:,:,:,f); 

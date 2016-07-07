e1d11476dc62839a6097d22f578722e579c26a19
lorenaphys <lorena.030789@gmail.com>
2016-07-06 19:17:58 -0500

    implementacion del valor de I usado en stress2016

--- 3D/isoformasifiu.m
+++ 3D/isoformasifiu.m
@@ -32,7 +32,7 @@ r = zeros(Nx,Ny,Nz);
 for i=1:Nx
     for j=1:Ny
         for k=1:Nz
-      r(i,j,k)=sqrt((i-Nx/2)^2+(j-Ny/2)^2+(k-Nz/2)^2);
+      r(i,j,k)=sqrt((i-Nx/2)^2+(j-Ny/2)^2+(k)^2);
       if r(i,j,k)>=15
       fi(i,j,k)=-1;
       end
@@ -59,9 +59,9 @@ Fm(:,:,:,1) = fi;
     [X,Y,Z]=meshgrid(1:Nx,1:Ny,1:Nz);
         teta=atan2((Y-Ny/2),(X-Nx/2));
         rad=sqrt((X-Nx/2+.5).^2+(Y-Ny/2+.5).^2);
-        %u=1.5*exp(-((X-Nx/2-.5).^2+(Y-Ny/2-.5).^2+(Z+35.5).^2)/50);
+        u=1.5*exp(-((X-Nx/2-.5).^2+(Y-Ny/2-.5).^2+(Z-7.5).^2)/50);
         %u=2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((-X+Nx/2-.5).^2+(-Y+Ny/2-.5).^2+(-Z+R+14).^2)/80));
-        u=-2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((X-Nx/2).^2+(Y-Ny/2).^2+(Z-R+14).^2)/50));%pueba 2, N=2
+        %u=-2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((X-Nx/2).^2+(Y-Ny/2).^2+(Z-R+14).^2)/50));%pueba 2, N=2
         %u=2.5*rand(Nx,Ny,Nz);
        
      Um(:,:,:,1) = u;
@@ -125,8 +125,10 @@ for iter=1:NF
 %         
 %         sigma=B/Bs;
         
-        I=120*(G)*sum(sum(sum((fi>=-.99))))/Nx/Ny/Nz;
-        I(fi<=0)=0;        
+        %I=120*(G)*sum(sum(sum((fi>=-.99))))/Nx/Ny/Nz;
+        I = 120*G;
+        I(abs(fi)>=.9)=0;
+        %I(fi<=0)=0;        
         
         fi=fi+dt*(Dfi*(lapF-lapFs)+I);
         u=u+dt*Du*(lapG);
@@ -164,4 +166,4 @@ end
 time = toc(t);
  
 
-save('julio6n.mat');
+save('julio6b*.mat');

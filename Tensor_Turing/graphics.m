8d35a6a45b0a83a2e126bd14401a0e3051daf4a2
lorenaphys <lorena.030789@gmail.com>
2016-07-12 15:30:42 -0500

    ultimas imagenes con el tensor de esfuerzos

--- Tensor_Turing/graphics.m
+++ Tensor_Turing/graphics.m
@@ -1,10 +1,10 @@
-load('julio11n.mat')
+load('julio11o.mat')
 
 fl=size(Fm);
 Nx = fl(1);
 Ny = fl(2);
 Nz = fl(3);
-f = 198;
+f = 56;
 M = struct('cdata',[],'colormap',[]);
 fi0(:,:,:) = Fm(:,:,:,1);
 fi(:,:,:)=Fm(:,:,:,f);
@@ -23,7 +23,7 @@ figure(1)
      set(p4,'FaceColor','interp','EdgeColor','none'),
      camlight, lighting phong
      axis equal, view(-16,24), axis off %de (18,10) lo cambie a (-16,24)%segundo cambio para view, antes era (-16,24)%cambié view de (-14,40) a (-16,24)
-     axis([1 40 1 40 1 70]),
+     axis([5 35 5 35 22 52]),
      %axis([1 Nx 1 Ny 1 Nz])
      colormap jet
      %colorbar
@@ -38,7 +38,7 @@ figure(2)
      set(p4,'FaceColor','interp','EdgeColor','none'),
      camlight, lighting phong
      axis equal, view(-16,24), axis off %de (18,10) lo cambie a (-16,24)%segundo cambio para view, antes era (-16,24)%cambié view de (-14,40) a (-16,24)
-     axis([1 40 1 40 1 70]),
+     axis([1 37 1 37 22 52]),
      %axis([1 Nx 1 Ny 1 Nz])
      colormap jet
      %colorbar
@@ -51,7 +51,7 @@ figure(3)
    hold on
    contour(fix0,[0 0],'r')
    axis equal
-   axis([1 22 1 40])
+   axis([18 52 1 40])
    %legend('\phi','\phi_0'),
     %getframe(gcf);
 %    hold off

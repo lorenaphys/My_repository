24b253920c50e4140742d2855c92faef68afbf84
lorenaphys <lorena.030789@gmail.com>
2016-07-29 12:00:14 -0500

    fi0:domo u0:rompimiento de simetria con N=2

--- Tensor_Turing/peli.m
+++ Tensor_Turing/peli.m
@@ -1,4 +1,4 @@
-load('julio28b*.mat');
+load('julio29d.mat');
 
 f = size(Um);
 Nx = f(1);
@@ -16,37 +16,37 @@ h = 101;
 %for k = cont2+cont4+2:cont2+cont4+2+cont5
 	%Fi(:,:,:,k) = Fm(:,:,:,cont2+cont4+1);
 %end
-M = struct('cdata',[],'colormap',[]);
-%M1 = struct('cdata',[],'colormap',[]);
+%M = struct('cdata',[],'colormap',[]);
+M1 = struct('cdata',[],'colormap',[]);
 
-%for i = 1:f(4)
-     %clf
-     %cdata = smooth3((Um(:,:,:,i)-min(min(min(Um(:,:,:,i)))))./...
-             %(max(max(max(Um(:,:,:,i))))-min(min(min(Um(:,:,:,i))))),'box',5);
-     %fi = smooth3(Fm(:,:,:,i),'box',5);
-     %p4=patch(isosurface(fi,0));
-     %isonormals(fi,p4);
-     %isocolors(cdata,p4);
-     %set(p4,'FaceColor','interp','EdgeColor','none'),
-     %camlight, lighting phong
-     %axis equal, view(-16,24), axis off 
-     %axis([1 40 1 40 1 30]),
-     %colormap jet
-     %M1(i) = getframe;
-     %disp(i)
-%end
-
-for k = 1:f(4)
-   R = 11;
-   u = Um(:,:,:,k); 
-    cdata = smooth3((Um(:,:,:,k)-min(min(min(Um(:,:,:,k)))))./...
-            (max(max(max(Um(:,:,:,k))))-min(min(min(Um(:,:,:,k))))),'box',5);
-    [x,y,z] = meshgrid(1:1:Nx,1:1:Ny,1:1:Nz);
-    xslice = [0,Nx/2];yslice = [0,Ny/2]; zslice = [0,10];
-    p3=slice(x,y,z,Um(:,:,:,k),xslice,yslice,zslice);
-    set(p3,'FaceColor','interp','EdgeColor','none','FaceAlpha',0.5),
-    axis equal, view(-70,20)
-    colormap jet,
-    M(k) = getframe;
-    disp(k)
+for i = 1:f(4)
+     clf
+     cdata = smooth3((Um(:,:,:,i)-min(min(min(Um(:,:,:,i)))))./...
+             (max(max(max(Um(:,:,:,i))))-min(min(min(Um(:,:,:,i))))),'box',5);
+     fi = smooth3(Fm(:,:,:,i),'box',5);
+     p4=patch(isosurface(fi,0));
+     isonormals(fi,p4);
+     isocolors(cdata,p4);
+     set(p4,'FaceColor','interp','EdgeColor','none'),
+     camlight, lighting phong
+     axis equal, view(-16,24), axis off 
+     axis([1 40 1 40 1 30]),
+     colormap jet
+     M1(i) = getframe;
+     disp(i)
 end
+
+%for k = 1:f(4)
+   %R = 11;
+   %u = Um(:,:,:,k); 
+    %cdata = smooth3((Um(:,:,:,k)-min(min(min(Um(:,:,:,k)))))./...
+            %(max(max(max(Um(:,:,:,k))))-min(min(min(Um(:,:,:,k))))),'box',5);
+    %[x,y,z] = meshgrid(1:1:Nx,1:1:Ny,1:1:Nz);
+    %xslice = [0,Nx/2];yslice = [0,Ny/2]; zslice = [0,10];
+    %p3=slice(x,y,z,Um(:,:,:,k),xslice,yslice,zslice);
+    %set(p3,'FaceColor','interp','EdgeColor','none','FaceAlpha',0.5),
+    %axis equal, view(-70,20)
+    %colormap jet,
+    %M(k) = getframe;
+    %disp(k)
+%end

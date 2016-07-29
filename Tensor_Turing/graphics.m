5344c2c61d90a9a3338e83820dd29b091339fdb6
lorenaphys <lorena.030789@gmail.com>
2016-07-29 01:39:23 -0500

    sigma^2=20

--- Tensor_Turing/graphics.m
+++ Tensor_Turing/graphics.m
@@ -1,10 +1,10 @@
-load('julio28a*.mat')
+load('julio29a.mat')
 
 fl=size(Fm);
 Nx = fl(1);
 Ny = fl(2);
 Nz = fl(3);
-f = 434;
+f = 284;
 M = struct('cdata',[],'colormap',[]);
 fi0(:,:,:) = Fm(:,:,:,1);
 fi(:,:,:)=Fm(:,:,:,f);

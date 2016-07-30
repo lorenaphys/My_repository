4270d7677bec39cdfc6f5b13802fa55cda193cf4
lorenaphys <lorena.030789@gmail.com>
2016-07-30 16:19:40 -0500

    cont2=251

--- Tensor_Turing/Turing.m
+++ Tensor_Turing/Turing.m
@@ -5,7 +5,7 @@ Nz = 70;
 
 %Contadores para el proceso
 cont1 = 2;
-cont2 = 201;
+cont2 = 251;
 cont3 = 200;
 cont4 = 30;
 cont5 = 20;
@@ -64,4 +64,4 @@ multifase(cont2+cont4+cont5+3,cont2+2*cont4+cont5+3,cont3);
 
 time = toc(t)/60;
 
-save('julio29a*.mat')
+save('julio30b.mat')

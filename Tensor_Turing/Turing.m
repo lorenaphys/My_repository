3b6f78fabe9355ec7b3f95ff448c836277f4acb3
lorenaphys <lorena.030789@gmail.com>
2016-07-30 23:27:38 -0500

    fi0:esfera u0:rompimiento de simetria con N=2

--- Tensor_Turing/Turing.m
+++ Tensor_Turing/Turing.m
@@ -17,7 +17,7 @@ r = zeros(Nx,Ny,Nz);
 for i=1:Nx
     for j=1:Ny
         for k=1:Nz
-      r(i,j,k)=sqrt((i-Nx/2)^2+(j-Ny/2)^2+(k)^2);
+      r(i,j,k)=sqrt((i-Nx/2)^2+(j-Ny/2)^2+(k-Nz/2)^2);
       if r(i,j,k)>=15
       fi(i,j,k)=-1;
       end
@@ -28,7 +28,7 @@ end
 
 %Condicion inicial para el morfogeno
 
-N = 5;
+N = 2;
 [X,Y,Z]=meshgrid(1:Nx,1:Ny,1:Nz);
 teta=atan2((Y-Ny/2),(X-Nx/2));
 rad=sqrt((X-Nx/2+.5).^2+(Y-Ny/2+.5).^2);
@@ -64,4 +64,4 @@ multifase(cont2+cont4+cont5+3,cont2+2*cont4+cont5+3,cont3);
 
 time = toc(t)/60;
 
-save('julio30i.mat')
+save('julio30j.mat')

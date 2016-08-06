e230c33f555f88e1af8e269cf2a77df165d0287a
lorenaphys <lorena.030789@gmail.com>
2016-08-05 20:33:56 -0500

    imagenes con C=0 x=(20,40/3,9)

--- Tensor_Turing/graphics.m
+++ Tensor_Turing/graphics.m
@@ -1,4 +1,4 @@
-load('julio25c.mat')
+load('julio27a.mat')
 
 fl=size(Fm);
 Nx = fl(1);
@@ -38,7 +38,7 @@ figure(2)
      set(p4,'FaceColor','interp','EdgeColor','none'),
      camlight, lighting phong
      axis equal, view(-16,24), axis off %de (18,10) lo cambie a (-16,24)%segundo cambio para view, antes era (-16,24)%cambié view de (-14,40) a (-16,24)
-     axis([4 35 4 35 1 15]),
+     axis([4 35 4 35 1 15.8]),
      %axis([1 Nx 1 Ny 1 Nz])
      colormap jet
      %colorbar

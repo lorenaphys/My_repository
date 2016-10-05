6a0a9388a2f7c5eeda093940e7f87704c47f313d
lorenaphys <lorena.030789@gmail.com>
2016-10-04 20:07:06 -0500

    NF=100 x=(20,20,6.5)

--- 3D/figure3D.m
+++ 3D/figure3D.m
@@ -1,10 +1,10 @@
-load('oct4b.mat')
+load('oct4c.mat')
 
 fl=size(Fm);
 Nx = fl(1);
 Ny = fl(2);
 Nz = fl(3);
-f = 61;
+f = 81;
 M = struct('cdata',[],'colormap',[]);
 fi(:,:,:)=Fm(:,:,:,f);
 u(:,:,:)=Um(:,:,:,f); 

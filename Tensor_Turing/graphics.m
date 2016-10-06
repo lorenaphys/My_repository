73f3b8110e4ecda4b80202faf2b24c1008e3a89a
lorenaphys <lorena.030789@gmail.com>
2016-10-06 00:34:53 -0500

    alpha=80

--- Tensor_Turing/graphics.m
+++ Tensor_Turing/graphics.m
@@ -1,10 +1,10 @@
-load('oct6k.mat')
+load('oct5a.mat')
 
 fl=size(Fm);
 Nx = fl(1);
 Ny = fl(2);
 Nz = fl(3);
-f = 34;
+f = 45;
 M = struct('cdata',[],'colormap',[]);
 fi0(:,:,:) = Fm(:,:,:,1);
 fi(:,:,:)=Fm(:,:,:,f);

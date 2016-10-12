3b3ad2e438d0df6d449583eac8fce0a7acc2c85f
lorenaphys <lorena.030789@gmail.com>
2016-10-12 13:26:39 -0500

    C=1.57, fi0:domo, u0:gaussiana con h=1.5, sigma^2=50 y x=(20,20,7), no se fija la base

--- Tensor_Turing/Turing.m
+++ Tensor_Turing/Turing.m
@@ -17,7 +17,7 @@ r = zeros(Nx,Ny,Nz);
 for i=1:Nx
     for j=1:Ny
         for k=1:Nz
-      r(i,j,k)=sqrt((i-Nx/2)^2+(j-Ny/2)^2+(k-Nz/2)^2);
+      r(i,j,k)=sqrt((i-Nx/2)^2+(j-Ny/2)^2+(k)^2);
       if r(i,j,k)>=15
       fi(i,j,k)=-1;
       end
@@ -32,9 +32,9 @@ N = 5;
 [X,Y,Z]=meshgrid(1:Nx,1:Ny,1:Nz);
 teta=atan2((Y-Ny/2),(X-Nx/2));
 rad=sqrt((X-Nx/2+.5).^2+(Y-Ny/2+.5).^2);
-%u=1.5*exp(-((X-Nx/2).^2+(Y-Ny/3).^2+(Z-9).^2)/50);
+u=1.5*exp(-((X-Nx/2).^2+(Y-Ny/2).^2+(Z-7).^2)/50);
 %u=2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((-X+Nx/2-.5).^2+(-Y+Ny/2-.5).^2+(-Z+R+14).^2)/80));
-u=-2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((X-Nx/2).^2+(Y-Ny/2).^2+(Z-1).^2)/50));
+%u=-2.5*rad.*(cos(teta*N)+sin(teta*N)).*(Z/Nz)/max(max(max(rad)))+(exp(-((X-Nx/2).^2+(Y-Ny/2).^2+(Z-1).^2)/50));
 %u=2.5*rand(Nx,Ny,Nz);
 %u=.1*u+.2*(rand(Nx,Ny,Nz)-.5);
 % = 1-2*u;
@@ -65,4 +65,4 @@ multifase(cont2+cont4+cont5+3,cont2+2*cont4+cont5+3,cont3);
 
 time = toc(t)/60;
 
-save('oct12a.mat')
+save('oct12b.mat')
--- Tensor_Turing/graphics.m
+++ Tensor_Turing/graphics.m
@@ -1,4 +1,4 @@
-load('oct12a.mat')
+load('oct12b.mat')
 
 fl=size(Fm);
 Nx = fl(1);
@@ -23,7 +23,7 @@ figure(1)
      set(p4,'FaceColor','interp','EdgeColor','none'),
      camlight, lighting phong
      axis equal, view(-16,24), axis off %de (18,10) lo cambie a (-16,24)%segundo cambio para view, antes era (-16,24)%cambié view de (-14,40) a (-16,24)
-     axis([3 38 3 38 19 52]),
+     axis([3 38 3 38 1 19]),
      %axis([1 Nx 1 Ny 1 Nz])
      colormap jet
      %colorbar
@@ -38,7 +38,7 @@ figure(2)
      set(p4,'FaceColor','interp','EdgeColor','none'),
      camlight, lighting phong
      axis equal, view(-16,24), axis off %de (18,10) lo cambie a (-16,24)%segundo cambio para view, antes era (-16,24)%cambié view de (-14,40) a (-16,24)
-     axis([3 38 3 38 19 52]),
+     axis([3 38 3 38 1 19]),
      %axis([1 Nx 1 Ny 1 Nz])
      colormap jet
      %colorbar
@@ -52,7 +52,7 @@ figure(3)
    hold on
    contour(fix0,[0 0],'r')
    axis equal
-   axis([15 55 1 38])
+   axis([1 22 1 38])
    %legend('\phi','\phi_0'),
     %getframe(gcf);
 %    hold off
